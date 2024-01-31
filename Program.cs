5e7693d73d2ce4a52b9f89c3edd3e4271371f125
Logan McNatt <logan.m.mcnatt@gmail.com>
2024-01-31 14:17:12 -0700

    Added gameboard

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
 ﻿// Tic-Tac-Toe Game
-
 using Helper;
 
 HelperTools ht = new HelperTools();
 
-string[] playerMoves = new string[9];
+string[] gameBoard = new string[9];
+bool gameOver = false;
 
 Console.WriteLine("Welcome to Tic-Tac-Toe!");
-Console.WriteLine("Player 1, Please make your first move");
+
+Console.WriteLine("Here are the options for the gameboard:\n" +
+                  "1 2 3\n" +
+                  "4 5 6\n" +
+                  "7 8 9");
+
+do
+{
+    Console.WriteLine("Player 1, Please make your first move");
+
+} while (!gameOver);
+
