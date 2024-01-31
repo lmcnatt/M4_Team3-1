11e4a18a25fb4435f7467ca352f9265dff0b2308
Logan McNatt <logan.m.mcnatt@gmail.com>
2024-01-31 14:42:36 -0700

    Added more Jazz

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ string[] gameBoard = new string[9];
 int player1Choice = 0;
 int player2Choice = 0;
 string player = "";
+int winner = 0;
 bool gameOver = false;
 
 Console.WriteLine("Welcome to Tic-Tac-Toe!");
@@ -28,5 +29,23 @@ Console.WriteLine("Player 2, Please make your first move");
 player1Choice = int.Parse(Console.ReadLine()) - 1;
 ht.printBoard(gameBoard, player);
 
+winner = ht.getWinner(gameBoard);
+
+if (winner == 0) // No win yet
+{
+    gameOver = false;
+}
+else if (winner == 1) // Player 1 won
+{
+    gameOver = true;
+}
+else if (winner == 2) // Player 2 won
+{
+    gameOver = true;
+}
+else if (winner == 3) // Tie
+{
+    gameOver = true;
+}
 // } while (!gameOver);
 
