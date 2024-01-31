9c0dfbd513cc538c8287c5375a8b0c9823ff0c42
Gracie Sentz <gs447@byu.edu>
2024-01-31 13:35:57 -0700

    helper.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// Tic-Tac-Toe Game
 
+using helper
+
+helper = new helper();
+
+string playerMoves = new string[9];
+
+Console.WriteLine("Welcome to Tic-Tac-Toe!")
+Console.WriteLine("Player 1, Please make your first move");
